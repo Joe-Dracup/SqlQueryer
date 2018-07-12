2c7d8d2db5783bd03864b612df631793fcd12ddd
Joe Dracup <32868247+Joe-Dracup@users.noreply.github.com>
2018-07-12 16:31:38 +0100

    Initial Commit

---
+++ UserIdLookup/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UserIdLookup.Services;
+
+namespace UserIdLookup
+{
+    public partial class Form1 : Form
+    {
+        DataAccessLayer DAL = new DataAccessLayer();
+        string userName = "";
+        DataTable dt = new DataTable();
+        DataView dv;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dt = DAL.findSqlUsers();
+            dataGridView1.DataSource = dt;
+        }
+
+        private void searchBox_TextChanged(object sender, EventArgs e)
+        {
+            userName = searchBox.Text;
+            dv = DAL.queryDataTable(userName, dt);
+            dataGridView1.DataSource = dv;
+        }
+    }
+}
---
+++ UserIdLookup/Services/DataAccessLayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace UserIdLookup.Services
+{
+    class DataAccessLayer
+    {
+        public DataTable findSqlUsers()
+        {
+            string connectionString = ConfigurationSettings.AppSettings["SqlConnectionString"].ToString();
+            string sql = $@"
+                SELECT userId,
+                fullname 
+                FROM [TABLE NAME]";         
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter dataadapter = new SqlDataAdapter(sql, connection);
+            DataTable dt = new DataTable();
+            connection.Open();
+            dataadapter.Fill(dt);
+            connection.Close();
+            return dt;  
+        }
+
+        public DataView queryDataTable(string userName, DataTable dt)
+        {;
+            DataView dv = new DataView(dt);
+            dv.RowFilter = $"(fullname like '%{userName}%')";
+            return dv;
+
+        }
+    }
+}
